a7a4be9c7c45de331a8af135e3fca4ed57fb066c
Peter Chiquetto <122702279+petersonchiquetto@users.noreply.github.com>
2025-04-19 22:33:43 -0300

    Update e correcao do docker com CI e CD

--- main.py
+++ main.py
@@ -1,12 +1,37 @@
-from fastapi import FastAPI
 import random
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get("/")
+
+class Estudante(BaseModel):
+    name: str
+    curso: str
+    ativo: bool
+
+
+@app.get("/helloword")
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/teste1")
+
+@app.get("/funcaoteste")
 async def funcaoteste():
-    return {"teste": True, "num_aleatorio": random.randint(0, 100)}
+    return {"teste": True, "num_aleatorio": random.randint(0, 2000)}
+
+
+@app.post("/estudantes/cadastro")
+async def create_estudante(estudante: Estudante):
+    return estudante
+
+
+@app.put("/estudantes/update/{id_estudatante}")
+async def update_item(id_estudante: int):
+    return id_estudante > 0
+
+
+@app.delete("/estudante/delete/{id_estudante}")
+async def delete_estudante(id_estudante: int):
+    return id_estudante > 0
+
